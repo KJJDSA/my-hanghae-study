8efd354e7695f3f8b5c93d92f5862b500926501c
Minjae Kim <x.xreesespuffs@gmail.com>
2022-10-23 17:38:06 +0900

    게시글 작성 안될 수 도 있습니다

--- routes/posts.js
+++ routes/posts.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const authMiddleware = require('../middleware/auth-middleware');
-require('dotenv').config();
 const upload = require('../middleware/upload-middleware');
 
 const PostsController = require('../controllers/posts');
+const uploadMiddleware = require('../middleware/upload-middleware');
 const postsController = new PostsController();
 
 router.get('/', postsController.getPosts);
 router.get('/:postId', postsController.getPostById);
 //이미지를 업로드받고 저장경로를 client에 전달해줌 미들웨어의 정적파일을 제공하는 라우터
-router.post('/', authMiddleware, upload.single('many'), postsController.createPost); //single, for문? db엔 어떻게,, //upload.array('many'),
+router.post('/', upload.array('many'), postsController.createPost); //single, for문? db엔 어떻게,, //upload.array('many'),
 router.put('/:postId', authMiddleware, postsController.updatePost);
 router.delete('/:postId', authMiddleware, postsController.deletePost);
 router.put('/likes/:postId', authMiddleware, postsController.likePost);
