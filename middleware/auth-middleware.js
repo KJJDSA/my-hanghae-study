e8468cd4b6ee0752f3ae16da3a8b19eefd976620
KJJDSA <kjjdsa@gmail.com>
2022-10-26 22:57:18 +0900

    authMiddleware try 위치 조정(자꾸 if 통과함) / fix bug

--- middleware/auth-middleware.js
+++ middleware/auth-middleware.js
@@ -3,14 +3,14 @@ const { User } = require("../models");
 // require('dotenv').config();
 
 module.exports = (req, res, next) => {
-    const authorization = req.headers.authorization;
-    if (!authorization) throw { message: "로그인 후 이용 가능한 기능입니다." };
-    const [authType, authToken] = (authorization || "").split(" ");
-    if (!authToken || authType !== "Bearer") throw { message: "옳지 않은 접근입니다." }
-
-    res.locals.authToken = authToken;
-
     try {
+        const authorization = req.headers.authorization;
+        if (!authorization) throw { message: "로그인 후 이용 가능한 기능입니다." };
+        const [authType, authToken] = (authorization || "").split(" ");
+        if (!authToken || authType !== "Bearer") throw { message: "옳지 않은 접근입니다." }
+
+        res.locals.authToken = authToken;
+
         const { userId } = jwt.verify(authToken, process.env.JWT_SECRET_KEY);
         User.findOne({ where: { userId } }).then((user) => {
             res.locals.user = user;
