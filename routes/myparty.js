99d5cd24e6e477954b1aec832b5a56c68d529e9d
Minjae Kim <x.xreesespuffs@gmail.com>
2022-11-02 07:54:23 +0900

    수정기능 제한 完

--- routes/myparty.js
+++ routes/myparty.js
@@ -6,7 +6,7 @@ const myPartyController = new MyPartyController();
 const authMiddleware = require("../middlewares/authmiddleware");
 
 // 전체 파티 정보 불러오기
-router.get("/", myPartyController.lookupMyParty);
+router.get("/", authMiddleware, myPartyController.lookupMyParty);
 
 // OTT 정보 수정하기
 router.put("/:partyId", authMiddleware, myPartyController.changePartyInfo);
