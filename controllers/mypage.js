ec426a109ee5dc31288464392b1663d05ae47ac2
Minjae Kim <x.xreesespuffs@gmail.com>
2022-11-02 02:13:15 +0900

    lookupMyParty 完

--- controllers/mypage.js
+++ controllers/mypage.js
@@ -50,10 +50,10 @@ class MyPageController {
     }
   };
 
-  DeleteBankAccount = async (req, res) => {
+  deleteBankAccount = async (req, res) => {
     try {
       const { userId } = res.locals.user;
-      await this.myPageService.DeleteBankAccount({
+      await this.myPageService.deleteBankAccount({
         userId,
       });
       res.status(200).json({ message: "삭제가 완료되었습니다." });
@@ -63,18 +63,23 @@ class MyPageController {
     }
   };
 
-
   /** ############# 카드 ############### */
   createCard = async (req, res, next) => {
     try {
       const { bank, card, MMYY, birth, password } = req.body;
       const { userId } = res.locals.user;
-      const createCard = await this.myPageService
-        .createCard({ bank, card, MMYY, birth, password, userId });
+      const createCard = await this.myPageService.createCard({
+        bank,
+        card,
+        MMYY,
+        birth,
+        password,
+        userId,
+      });
       res.status(201).json({ data: createCard });
     } catch (error) {
       return res.status(400).send({
-        errorMessage: error.name + ':' + error.message,
+        errorMessage: error.name + ":" + error.message,
       });
     }
   };
@@ -89,7 +94,7 @@ class MyPageController {
       return res.status(200).json({ data: cards });
     } catch (error) {
       return res.status(400).send({
-        errorMessage: error.name + ':' + error.message,
+        errorMessage: error.name + ":" + error.message,
       });
     }
   };
@@ -99,12 +104,19 @@ class MyPageController {
       const { BankCardId } = req.params;
       const { bank, card, MMYY, birth, password } = req.body;
       const { userId } = res.locals.user;
-      const cardEdit = await this.myPageService
-        .cardEdit({ bank, card, MMYY, birth, password, userId, BankCardId });
+      const cardEdit = await this.myPageService.cardEdit({
+        bank,
+        card,
+        MMYY,
+        birth,
+        password,
+        userId,
+        BankCardId,
+      });
       res.status(200).json({ data: cardEdit });
     } catch (error) {
       return res.status(400).send({
-        errorMessage: error.name + ':' + error.message,
+        errorMessage: error.name + ":" + error.message,
       });
     }
   };
@@ -113,15 +125,17 @@ class MyPageController {
     try {
       const { BankCardId } = req.params;
       const { userId } = res.locals.user;
-      const cardDelete = await this.myPageService.cardDelete({ BankCardId, userId });
+      const cardDelete = await this.myPageService.cardDelete({
+        BankCardId,
+        userId,
+      });
       res.status(200).json({ data: cardDelete });
     } catch (error) {
       return res.status(400).send({
-        errorMessage: error.name + ':' + error.message,
+        errorMessage: error.name + ":" + error.message,
       });
     }
   };
-
 }
 
 module.exports = MyPageController;
