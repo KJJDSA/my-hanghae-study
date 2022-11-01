cdee3d5423063271d39c667503f69c1772fa2291
KJJDSA <kjjdsa@gmail.com>
2022-11-01 15:55:25 +0900

    myparty authmiddlware 적용

--- routes/myparty.js
+++ routes/myparty.js
@@ -3,11 +3,12 @@ const router = express.Router();
 
 const MyPartyController = require('../controllers/myparty');
 const myPartyController = new MyPartyController;
+const authMiddleware = require("../middlewares/authmiddleware");
 
 // 전체 파티 정보 불러오기
-router.get('/', myPartyController.lookupMyParty);
+router.get('/', authMiddleware, myPartyController.lookupMyParty);
 
 // OTT 정보 수정하기
-router.put('/:partyId', myPartyController.changePartyInfo);
+router.put('/:partyId', authMiddleware, myPartyController.changePartyInfo);
 
 module.exports = router;
