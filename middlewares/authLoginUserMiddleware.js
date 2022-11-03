8abbb2a88b430466aa292666bbe8d91deba4c509
KJJDSA <kjjdsa@gmail.com>
2022-11-03 10:45:29 +0900

    로그인 미들웨어 res.headers 주석처리

--- middlewares/authLoginUserMiddleware.js
+++ middlewares/authLoginUserMiddleware.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 // 로그인 되어 있는 유저일 경우 Error를 반환한다.
 module.exports = (req, res, next) => {
   try {
-    const cookies = req.headers[process.env.cookie_name] || req.cookies[process.env.COOKIE_NAME];
+    const cookies = /**req.headers[process.env.cookie_name] ||*/ req.cookies[process.env.COOKIE_NAME];
     if (cookies) {
       return res.status(403).send({
         errorMessage: '이미 로그인이 되어있습니다.',
