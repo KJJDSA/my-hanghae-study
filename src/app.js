450ac4faff6b0300a014b6c018659223f5b339c2
cesdea <aprkvnem@gmail.com>
2022-11-09 22:29:21 +0900

    add(databaseLog):데이터베이스 연결시 로그 생성

--- src/app.js
+++ src/app.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const app = express();
+const { sequelize } = require("../models");
 const indexRouter = require("./routes");
 require("dotenv").config();
 const env = process.env;
 
+sequelize
+  .sync({ force: false })
+  .then(() => {
+    console.log("데이터베이스 연결 성공");
+  })
+  .catch((err) => {
+    console.error(err);
+  });
 
+app.use(express.json())
 app.use("/api", indexRouter);
 // app.use(express.static('public'))
 
