7f318d0d91844e835ab4f0481577eec401873b99
KJJDSA <kjjdsa@gmail.coom>
2022-11-15 01:40:02 +0900

    feat(search): 검색시 반환하는 데이터 종류 줄임. 도움되는 리뷰순으로 무작위 정렬

--- src/repositories/gamesrepository.js
+++ src/repositories/gamesrepository.js
@@ -9,6 +9,7 @@ module.exports = class SteamSearchRepository {
       // 띄어쓰기한 모든 키워드가 존재하는 정확한 검색결과를 표시함
       const game_list = await Games.findAll({
         raw: true,
+        attributes: ["id", "name", "review_score", "review_score_desc", "total_positive", 'total_negative', "img_url"],
         where: {
           [Op.and]: [
             keywords_deformed,
@@ -16,14 +17,28 @@ module.exports = class SteamSearchRepository {
           ]
         },
         include: [
-          { model: Reviews },
+          {
+            model: Reviews,
+            attributes: [
+              "playtime_at_review",
+              "language",
+              "review",
+              "timestamp_updated",
+              "voted_up",
+              "votes_up",
+              "votes_funny",
+              "weighted_vote_score"]
+          },
           {
             model: Metascores,
             attributes: ["meta_score", "user_review"]
           }
         ],
-
       })
+      game_list.sort((a, b) => {
+        return b["Reviews.weighted_vote_score"] - a["Reviews.weighted_vote_score"]
+      })
+
       // // 키워드 중 하나라도 맞으면 검색결과를 불러오게 하려했으나 'the', 'a' 같은 건 너무 길게 불러옴. 봉인! 
       // const game_list_incorrect = await Games.findAll({
       //   raw: true,
