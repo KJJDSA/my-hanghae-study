f8eac8b4476d022e346bde161e122ecaf0116f94
cesdea <aprkvnem@gmail.com>
2022-11-15 11:33:05 +0900

    feat(search):log 파일 생성

--- src/controllers/steamsearchcontroller.js
+++ src/controllers/steamsearchcontroller.js
@@ -1,22 +1,27 @@
 const SteamSearchService = require("../services/steamsearchservice");
+let logger = require('../middlewares/log/searchlogger');
 
 module.exports = class SteamSearchController {
-  steamSearchService = new SteamSearchService();
-  steamSearch = async (req, res) => {
-    try {
-      // 쿼리스트링으로 받음
-      const { keyword } = req.query;
-      // console.log(keyword)
-      const keywords = keyword.split(" ");
-      const { game_list } = await this.steamSearchService.steamSearch({
-        keywords,
-      });
-      res.render("search", {
-        data: game_list,
-      });
-    } catch (error) {
-      console.log(error);
-      res.status(400).json({ Type: error.name, Message: error.message });
+    steamSearchService = new SteamSearchService();
+    steamSearch = async (req, res) => {
+        try {
+            //테스트코드
+            const user="testUser";
+            // 쿼리스트링으로 받음
+            const { keyword } = req.query
+            //로깅
+            logger.info({label:'GET /api/search/',message:user+"-"+keyword})
+            // console.log(keyword)
+            const keywords = keyword.split(" ")
+            const { game_list } = await this.steamSearchService.steamSearch({ keywords });
+            res.status(200).json({
+                data: game_list,
+                // review_list: result.review_list
+            });
+        } catch (error) {
+            console.log(error);
+            res.status(400).json({ Type: error.name, Message: error.message });
+        }
+
     }
-  };
 };
