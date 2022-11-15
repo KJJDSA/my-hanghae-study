14abf88e58fb03e6a511418dfaf8296fdadf5e12
Minjae Kim <x.xreesespuffs@gmail.com>
2022-11-15 09:19:31 +0900

    3rd 9:19am

--- src/app.js
+++ src/app.js
@@ -1,13 +1,21 @@
 const express = require("express");
 const app = express();
 const routes = require("./routes");
+const ejs = require("ejs");
+const path = require("path");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public"));
 
+app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
-app.set("views", __dirname + "/views");
+app.engine("html", ejs.renderFile);
+
+app.use(express.static(__dirname + "public"));
+
+app.get("/", function (req, res) {
+  res.render("index");
+});
 
 app.use("/", routes);
 
