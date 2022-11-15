00db09a341f5bc11c55eace172d1c2c73f78a4fa
Minjae Kim <x.xreesespuffs@gmail.com>
2022-11-15 09:43:35 +0900

    서치 컨트롤러 리턴값 수정

--- src/controllers/steamsearchcontroller.js
+++ src/controllers/steamsearchcontroller.js
@@ -1,23 +1,22 @@
 const SteamSearchService = require("../services/steamsearchservice");
 
 module.exports = class SteamSearchController {
-    steamSearchService = new SteamSearchService();
-    steamSearch = async (req, res) => {
-        try {
-            // 쿼리스트링으로 받음
-            const { keyword } = req.query
-            // console.log(keyword)
-            const keywords = keyword.split(" ")
-            const { game_list } = await this.steamSearchService.steamSearch({ keywords });
-            res.status(200).json({
-                data: game_list,
-                // review_list: result.review_list
-            });
-        } catch (error) {
-            console.log(error);
-            res.status(400).json({ Type: error.name, Message: error.message });
-        }
-
+  steamSearchService = new SteamSearchService();
+  steamSearch = async (req, res) => {
+    try {
+      // 쿼리스트링으로 받음
+      const { keyword } = req.query;
+      // console.log(keyword)
+      const keywords = keyword.split(" ");
+      const { game_list } = await this.steamSearchService.steamSearch({
+        keywords,
+      });
+      res.render("search", {
+        data: game_list,
+      });
+    } catch (error) {
+      console.log(error);
+      res.status(400).json({ Type: error.name, Message: error.message });
     }
   };
 };
