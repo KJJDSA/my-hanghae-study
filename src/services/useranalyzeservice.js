2470d29b7c9d625f8b8e36d8b0a929c1d1d6bbe7
cesdea <aprkvnem@gmail.com>
2022-11-16 10:58:14 +0900

    feat(analyze):구조 분할, 유저 추천

--- src/services/useranalyzeservice.js
+++ src/services/useranalyzeservice.js
@@ -1,5 +1,45 @@
 // const UserAnalyzeRepository = require("../repositories/useranalyzerepository");
+const fs=require('fs').promises;
+const path = require("path");
 
 module.exports = class UserAnalyzeService {
     // userAnalyzeRepository = new UserAnalyzeRepository();
+    bestGameList=async()=>{
+        let gameList=new Object();
+        const dir=path.join(__dirname,'..','..','logs','/users/')
+        let filelist=await fs.readdir(dir)// 하나의 데이터씩 나누어 출력
+        for(let file of filelist){
+            let list=await fs.readFile(path.join(__dirname,'..','..','logs','/users/')+file,{encoding:'utf8'});
+            for(let ele of list.split('\n')){
+                if(ele!==""){
+                    let id_json=JSON.parse(ele)
+                    for(let id of id_json.game_appid){
+                        if(gameList[id.appid]===undefined){
+                            gameList[id.appid]=1
+                        }else{
+                            gameList[id.appid]++;
+                        }
+                    }
+                }
+            }
+        }
+        return gameList;
+    }
+    userBestGameList=async({user_id})=>{
+        let gameList=new Object();
+        let list=await fs.readFile(path.join(__dirname,'..','..','logs','/users/')+user_id+'.log',{encoding:'utf8'});
+        for(let ele of list.split('\n')){
+            if(ele!==""){
+                let id_json=JSON.parse(ele)
+                for(let id of id_json.game_appid){
+                    if(gameList[id.appid]===undefined){
+                        gameList[id.appid]=1
+                    }else{
+                        gameList[id.appid]++;
+                    }
+                }
+            }
+        }
+        return gameList;
+    }
 };
