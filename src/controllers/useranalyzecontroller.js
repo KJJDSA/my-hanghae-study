efdcf742268ece9f056a8b1190d076f184670ed8
KJJDSA <kjjdsa@gmail.coom>
2022-11-17 19:27:18 +0900

    feat(analyze): 추천기능 프로토타입 완성. 대문 전체추천 기능

--- src/controllers/useranalyzecontroller.js
+++ src/controllers/useranalyzecontroller.js
@@ -2,19 +2,19 @@ const UserAnalyzeService = require("../services/useranalyzeservice");
 
 module.exports = class UserAnalyzeController {
     userAnalyzeService = new UserAnalyzeService();
-    bestGame=async(req,res,next)=>{
+    bestGame = async (req, res, next) => {
         try {
-            const best_list=await this.userAnalyzeService.bestGameList();
-            return res.status(200).json({best_list})
+            const best_list = await this.userAnalyzeService.bestGameList();
+            return res.status(200).json({ best_list })
         } catch (error) {
             next(error)
         }
     }
-    userBestGame=async(req,res,next)=>{
+    userBestGame = async (req, res, next) => {
         try {
-            const {user_id}=req.params;
-            const user_best_list=await this.userAnalyzeService.userBestGameList({user_id});
-            return res.status(200).json({user_best_list})
+            const { user_id } = req.params;
+            const user_best_list = await this.userAnalyzeService.userBestGameList({ user_id });
+            return res.status(200).json({ user_best_list })
         } catch (error) {
             next(error)
         }
--- src/services/useranalyzeservice.js
+++ src/services/useranalyzeservice.js
@@ -1,24 +1,28 @@
 // const UserAnalyzeRepository = require("../repositories/useranalyzerepository");
-const fs=require('fs').promises;
+const GamesRepository = require("../repositories/gamesrepository");
+const fs = require('fs').promises;
 const path = require("path");
+const { Games, Reviews, Metascores } = require("../../models");
+const { Op } = require("sequelize");
 
 module.exports = class UserAnalyzeService {
+    gamesRepository = new GamesRepository();
     // userAnalyzeRepository = new UserAnalyzeRepository();
-    bestGameList=async()=>{
+    bestGameList = async () => {
         try {
-            let gameList=new Object();
-            const dir=path.join(__dirname,'..','..','logs','/users/')
-            let filelist=await fs.readdir(dir)// 하나의 데이터씩 나누어 출력
-            for(let file of filelist){
-                let list=await fs.readFile(path.join(__dirname,'..','..','logs','/users/')+file,{encoding:'utf8'});
-                for(let ele of list.split('\n')){
-                    if(ele!==""){
-                        let id_json=JSON.parse(ele)
-                        if(id_json['game_appid']!==undefined){
-                            for(let id of id_json.game_appid){
-                                if(gameList[id.appid]===undefined){
-                                    gameList[id.appid]=1
-                                }else{
+            let gameList = new Object();
+            const dir = path.join(__dirname, '..', '..', 'logs', '/users/')
+            let filelist = await fs.readdir(dir)// 하나의 데이터씩 나누어 출력
+            for (let file of filelist) {
+                let list = await fs.readFile(path.join(__dirname, '..', '..', 'logs', '/users/') + file, { encoding: 'utf8' });
+                for (let ele of list.split('\n')) {
+                    if (ele !== "") {
+                        let id_json = JSON.parse(ele)
+                        if (id_json['game_appid'] !== undefined) {
+                            for (let id of id_json.game_appid) {
+                                if (gameList[id.appid] === undefined) {
+                                    gameList[id.appid] = 1
+                                } else {
                                     gameList[id.appid]++;
                                 }
                             }
@@ -26,50 +30,91 @@ module.exports = class UserAnalyzeService {
                     }
                 }
             }
-            let counting=new Object();
-            for(let i in gameList){
-                if(counting[gameList[i]]===undefined){
-                    counting[gameList[i]]=new Array();
+            let counting = new Object();
+            for (let i in gameList) {
+                if (counting[gameList[i]] === undefined) {
+                    counting[gameList[i]] = new Array();
                     counting[gameList[i]].push(i);
-                }else{
+                } else {
                     counting[gameList[i]].push(i);
                 }
             }
-            return counting;
+            // 몇번 검색했는지 주려면 객체로 올려보내야해서 복잡해진다. 
+            // 3개만 추려서 주고싶다.
+            // 최상위가 3개 이하면 그 다음걸 주고싶다.
+            let array = []
+            for (const i in counting) {
+                array.push(i)
+            }
+            const max = Math.max(...array);
+            const keywords_deformed = []
+            if (counting[max].length < 3) {
+                // 가장 많이 클릭한 숫자의 배열을 찾아 return
+                for (const appid of counting[max]) { keywords_deformed.push({ appid }) }
+                const options = {
+                    where: {
+                        [Op.and]: [
+                            { [Op.or]: keywords_deformed },
+                            // 이미지가 없으면 안뽑아옴
+                            { img_url: { [Op.not]: null } }
+                        ]
+                    }
+                }
+                const { game_list } = await this.gamesRepository.findGames({ options });
+                return game_list
+            } else if (counting[max].length > 3) {
+                // 만약 최고 검색숫자의 배열이 3개를 안넘으면 두번째것도 표시
+                const arr = array.filter(x => {
+                    if (x !== (max + "")) return x
+                })
+                const max_secound = Math.max(...arr);
+                const first_second = [...counting[max], ...counting[max_secound]]
+                for (const appid of first_second) { keywords_deformed.push({ appid }); }
+                const options = {
+                    where: {
+                        [Op.and]: [
+                            { [Op.or]: keywords_deformed },
+                            { img_url: { [Op.not]: null } }
+                        ]
+                    }
+                }
+                const { game_list } = await this.gamesRepository.findGames({ options });
+                return game_list
+            }
         } catch (error) {
-            throw(error)
+            throw (error)
         }
     }
-    userBestGameList=async({user_id})=>{
+    userBestGameList = async ({ user_id }) => {
         try {
-            let gameList=new Object();
-            let list=await fs.readFile(path.join(__dirname,'..','..','logs','/users/')+user_id+'.log',{encoding:'utf8'});
-            for(let ele of list.split('\n')){
-                if(ele!==""){
-                    let id_json=JSON.parse(ele)
-                    if(id_json['game_appid']!==undefined){
-                        for(let id of id_json.game_appid){
-                            if(gameList[id.appid]===undefined){
-                                gameList[id.appid]=1
-                            }else{
+            let gameList = new Object();
+            let list = await fs.readFile(path.join(__dirname, '..', '..', 'logs', '/users/') + user_id + '.log', { encoding: 'utf8' });
+            for (let ele of list.split('\n')) {
+                if (ele !== "") {
+                    let id_json = JSON.parse(ele)
+                    if (id_json['game_appid'] !== undefined) {
+                        for (let id of id_json.game_appid) {
+                            if (gameList[id.appid] === undefined) {
+                                gameList[id.appid] = 1
+                            } else {
                                 gameList[id.appid]++;
                             }
                         }
                     }
                 }
             }
-            let counting=new Object();
-            for(let i in gameList){
-                if(counting[gameList[i]]===undefined){
-                    counting[gameList[i]]=new Array();
+            let counting = new Object();
+            for (let i in gameList) {
+                if (counting[gameList[i]] === undefined) {
+                    counting[gameList[i]] = new Array();
                     counting[gameList[i]].push(i);
-                }else{
+                } else {
                     counting[gameList[i]].push(i);
                 }
             }
             return counting;
         } catch (error) {
-            throw(error)
+            throw (error)
         }
     }
 };
