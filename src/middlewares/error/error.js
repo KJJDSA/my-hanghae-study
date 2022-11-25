fdcaa713b0f69cc58f699adb14e1bcc8e8369639
KJJDSA <kjjdsa@gmail.coom>
2022-11-25 19:38:04 +0900

    fix(파일이름변경)

--- src/middlewares/error/error.js
+++ src/middlewares/error/error.js
@@ -1,19 +1,18 @@
-let {errorLog} = require('../log/errorlogger');
+let { errorLog } = require('../log/error_logger');
 const error404 = (req, res, next) => {
-    const error = new Error(`${req.url} ${req.method} 존재하지 않습니다.`);
-    error.status = 404;
-    next(error);
-  };
-  
-  const error = (err, req, res, next) => {
-    res.locals.message = err.message;
-    res.locals.error = err;
-    errorLog.error({label:err.status||500,message:err.name+"-"+err.message||'null'})
-    res.status(err.status || 500).send(err.message);
-  };
-  
-  module.exports = {
-    error404,
-    error,
-  };
-  
+  const error = new Error(`${req.url} ${req.method} 존재하지 않습니다.`);
+  error.status = 404;
+  next(error);
+};
+
+const error = (err, req, res, next) => {
+  res.locals.message = err.message;
+  res.locals.error = err;
+  errorLog.error({ label: err.status || 500, message: err.name + "-" + err.message || 'null' })
+  res.status(err.status || 500).send(err.message);
+};
+
+module.exports = {
+  error404,
+  error,
+};
